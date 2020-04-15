eacaa6009affb434e5f532e2fcc23b0af6d2f87c
Tom Nguyen <huuthong103@gmail.com>
2020-04-15 00:33:17 -0700

    add api, fetch data, display for global data, working on a select country

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
+
 class App extends React.Component{
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData })
+    }
+
+    handleCountryChange = async (country) => {
+        console.log(country)
+    }
+    
     render(){
+        const { data } = this.state;
         return (
             <div className={styles.container}>
-                <Cards />
-                <CountryPicker />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart />
             </div>
         )
